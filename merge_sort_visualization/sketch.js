afb0f75a8163ee4a5ab17ce1b166677d91b14c84
MadsAnker <madsanker@outlook.com>
2019-04-11 14:35:01 +0200

    Add merge sort visualization

---
+++ merge_sort_visualization/sketch.js
@@ -0,0 +1,92 @@
+var ll = 1000;
+var numRange = 1000;
+var drawList;
+var waitTime = 1;
+var osc = new p5.TriOsc();
+
+
+async function merge(arr, start, middle, end) {
+	var ll = middle-start;
+	var rl = end-middle;
+
+	var l = [];
+	var r = [];
+	for (var i = 0; i < ll; i++) {
+		l[i] = arr[start+i];
+	}
+	for (var i = 0; i < rl; i++) {
+		r[i] = arr[middle+i];
+	}
+
+	var put = start;
+	var lindex = 0;
+	var rindex = 0;
+
+	while (lindex < ll && rindex < rl) {
+		if (r[rindex] < l[lindex]) {
+			arr[put] = r[rindex];
+			rindex++;
+		} else {
+			arr[put] = l[lindex];
+			lindex++;
+		}
+		osc.freq(map(arr[put], 0, numRange, 100, 1000));
+		put++;
+		await sleep(waitTime);
+	}
+
+	if (rindex < rl) {
+		for (; rindex < rl; rindex++) {
+			arr[put] = r[rindex];
+			osc.freq(map(arr[put], 0, numRange, 100, 1000));
+			put++;
+			await sleep(waitTime);
+		}
+	} else {
+		for (; lindex < ll; lindex++) {
+			arr[put] = l[lindex];
+			osc.freq(map(arr[put], 0, numRange, 100, 1000));
+			put++;
+			await sleep(waitTime);
+		}
+	}
+}
+
+async function mergeSort(arr, start, end) {
+	if (end-start < 2)
+		return;
+
+	var middle = Math.floor((start+end)/2);
+	
+	
+	await mergeSort(arr, start, middle);
+	await mergeSort(arr, middle, end);
+
+	await merge(arr, start, middle, end);
+}
+
+function setup() {
+	drawList = Array(ll);
+	createCanvas(1600, 900);
+	frameRate(30);
+	stroke(255);
+	for (var i = 0; i < ll; i++) {
+		drawList[i] = int(random(1, numRange));
+	}
+	osc.start();
+	mergeSort(drawList, 0, drawList.length);
+}
+
+function draw() {
+	background(255);
+	strokeWeight(0);
+	for (var i = 0; i < ll; i++) {
+		var h = drawList[i]*height/numRange;
+		fill(0, 200, 100);
+		rect(width/ll*i, height-h, width/ll, h);
+	}
+}
+
+const sleep = (milliseconds) => {
+	return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
